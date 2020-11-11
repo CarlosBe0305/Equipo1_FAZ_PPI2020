f705b506894f58a9b4c15a257b0dba7931100b4c
mariapaulinarios <66378453+mariapaulinarios@users.noreply.github.com>
2020-11-11 09:22:55 -0500

    Update routes.js

--- Back-End/routes/routes.js
+++ Back-End/routes/routes.js
@@ -5,7 +5,35 @@ const mysqlConnection = require('../db/db');
 
 router.get('/usuarios', (req,res) => {
 
-  mysqlConnection.query('SELECT * FROM Usuarios ', (err, rows, fields) => {
+  mysqlConnection.query('SELECT * FROM Usuarios', (err, rows, fields) => {
+    if (!err) {
+      res.json(rows);
+    } else {
+      console.log(err);
+    }
+  });
+
+});
+
+router.put('/usuarios/:ID', (req,res) => {
+
+  const{UserName,Correo,Telefono} = req.body;
+
+  const{ ID } = req.params;
+  mysqlConnection.query(`UPDATE Usuarios SET UserName = ?,Correo = ?,Telefono = ? WHERE ID = ?`,[ID,UserName,Correo,Telefono], 
+  (err, rows, fields) => {
+    if (!err) {
+      res.json({status: 'Estudiante actualizado'});
+    } else {
+      console.log(err);
+    }
+  });
+
+});
+
+router.get('/baches', (req,res) => {
+
+  mysqlConnection.query('SELECT * FROM Baches', (err, rows, fields) => {
     if (!err) {
       res.json(rows);
     } else {
@@ -29,5 +57,22 @@ router.post('/nuevo-usuario',(req,res) => {
     res.json({message: `Usuario registrado`, })
   });
   });
+
+  router.post('/nuevo-bache',(req,res) => {
+
+  const {ID,ID_Usuario, Latitude,Longitude,Nivel_Gravedad,Velocidad,ValorX_Acel,ValorY_Acel,ValorZ_Acel} = req.body;
+  
+  let bache = [ID,ID_Usuario, Latitude,Longitude,Nivel_Gravedad,Velocidad,ValorX_Acel,ValorY_Acel,ValorZ_Acel];
+
+  let nuevobache = `INSERT INTO Baches(ID,ID_Usuario, Latitude,Longitude,Nivel_Gravedad,Velocidad,ValorX_Acel,ValorY_Acel,ValorZ_Acel) VALUES(?,?,?,?,?,?,?,?,?)`;
+
+  mysqlConnection.query(nuevobache, bache, (err, results, fields) => {
+    if (err) {
+      return console.error(err.mesage);
+    } 
+    res.json({message: `Bache registrado`, })
+  });
+  });
+
 
 module.exports = router;
